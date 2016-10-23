a4cf143f6e3f5948f6bc27c33cac4cd2e676a7bd
Nikhil Patwardhan <nikhil.birdwatcher@gmail.com>
2016-10-22 21:52:00 -0400

    More pythonic way of Beeler1972Big

--- calcs/beeler1972big.py
+++ calcs/beeler1972big.py
@@ -4,6 +4,10 @@ Created on Sat Oct 22 10:14:56 2016
 
 Ref: https://crypto.stanford.edu/pbc/notes/pi/code.html
 
+Compute Pi to arbitrary precision
+N is the number of iterations of the outer for loop. The precision is directly
+dependent on N (4*N).
+
 @author: Nikhil
 """
 import logging
@@ -15,39 +19,35 @@ from base import BaseCalculator
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 class Beeler1972Big(BaseCalculator):
     
     def __init__(self):
-        super(BaseCalculator, self).__init__()
+        super(Beeler1972Big, self).__init__()
     
-    def compute(self, N=250):
+    def compute(self, N=1):
         N, res = N*14, 0.
         getcontext().prec = N*4
-        s = None
+        s = StringIO()
         
         try:
-            s = StringIO()
             s.write('.')
             r = [2000] * (N+1)
             c = 0
             
             for k in xrange(N, 1, -14):
                 d = 0
-                i = k
-                while True:
+                for i in xrange(k, 0, -1):
                     d += r[i] * 10000
                     b = 2 * i - 1
                     r[i] = d % b
                     d /= b
                     i -= 1
-                    if not i:
-                        break
-                    d *= i
+                    d *= (i or 1)
                     
                     logger.debug(r)
-                    logger.debug('d=%d, b=%d, i=%d, c=%d', d, b, i, c)
+                    logger.debug('d=%d, b=%d, i=%d, c=%d', d, b, i or 1, c)
                 
                 _s = '%04d' % (c + d/10000)
                 s.write(_s)
